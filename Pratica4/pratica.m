bd55f1e012e085112b6689f3c05580f216fae2c3
GuerraUTFPR <guerramatheus2@gmail.com>
2017-09-22 23:32:41 -0300

    Pratica 4 concluida

--- Pratica4/pratica.m
+++ Pratica4/pratica.m
@@ -16,14 +16,29 @@ mascara = mascara * 1/81;
 
 saida = filter2(mascara,imagem);
 
+#imshow(saida);
+imwrite(saida, "1.jpg"); 
 
-imshow(saida);
 # A imagem ficou levemente borrada e com uma borda preta, 
 # pois o filtro usou pixels que estao alem da imagem, ou seja, 
-# as partes que ficaram pra fora da imagem recebe o valor zero pois não h´a um 3º parametro na filter2.
+# as partes que ficaram pra fora da imagem recebe o valor zero pois não ha um 3º parametro na filter2.
 
 imagem2 = padarray(imagem, [4,4], "replicate");
 
 saida2 = filter2(mascara,imagem2, "valid");
 
-imshow(saida2);
+#imshow(saida2);
+imwrite(saida2, "2.jpg"); 
+
+magnitude_x = [-1, 0, 1; -2, 0, 2; -1, 0, 1];
+magnitude_y = [1, 2, 1; 0, 0, 0; -1, -2, -1];
+
+imagem3 = padarray(imagem, [3,3], "replicate");
+
+gradiente_x = filter2(magnitude_x, imagem3, "valid");
+gradiente_y = filter2(magnitude_y, imagem3, "valid");
+
+saida3 = sqrt(gradiente_x.^2 + gradiente_y.^2);
+
+imshow(saida3);
+imwrite(saida3, "3.jpg"); 
